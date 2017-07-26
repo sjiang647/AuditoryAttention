5911c59a2e9db4dc377cc755247ec20c421398cc
Andy Peng <andypg20@gmail.com>
2017-07-26 14:15:44 -0700

    Added matlab file

---
+++ attention.m
@@ -0,0 +1,23 @@
+%% Objective: Test effects of attention on auditory ensemble perception
+
+clear all;
+close all;
+clc;
+
+%% Constants and global variables
+
+%% Load/trim auditory noise stimuli
+
+%% Generate auditory tone stimuli
+
+%% Subject data input
+
+%% Task instructions
+
+%% Stimuli display (experiment)
+
+%% Keyboard input
+
+%% Data analysis
+
+%% Save data
