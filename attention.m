5d72416ff1c16691dbff2cde12c30f98c6fde901
mantejpanesar <mantejsingh1234@gmail.com>
2017-07-27 14:02:57 -0700

    commit

--- attention.m
+++ attention.m
@@ -58,10 +58,6 @@ for k = 1:127
 end
 
 %% Counterbalance conditions
-
-
-%counterbalancing [3, numTrials] what to ask, test tone high or lower, what to focus on 
-
 for i = 1:numTrials
     askWhat = mod(randperm(numTrials), 2); % 1 if mean, 0 if word
     highLow = mod(randperm(numTrials), 2); % 1 if high, 0 if low
@@ -72,11 +68,6 @@ for i = 1:numTrials
 end
 
 subjectData{5} = counterbalancing;
-
-% matrix of 3xNumTrials
-%numtrials 1 column --> what to ask 
-% 2nd column --> high or lower than th test tone 
-% 3rd column --> what they focus on 
 
 %% Subject data input
 
