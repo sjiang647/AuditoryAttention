ef08d57b1d5296276b4e6dfdf1149326deaca728
mantejpanesar <mantejsingh1234@gmail.com>
2017-07-27 21:37:32 -0700

    fixed bug

--- attention.m
+++ attention.m
@@ -255,7 +255,9 @@ Screen('Flip', window);
             % Ask for number of times words played
             while true
                 ans = Ask(window, ['How  many times was ' names(randsample(3,1)).name ' played (1-6): '], [],[], 'GetChar', RectLeft, RectTop, 25);
-                if (ans=='1')||(ans=='2')||(ans=='3')||(ans=='4')||(ans=='5')||(ans=='6')
+                if length(ans)>1
+                    ans = Ask(window, ['How  many times was ' names(randsample(3,1)).name ' played (1-6): '], [],[], 'GetChar', RectLeft, RectTop, 25);
+                else
                     break;
                 end
             end
